32c09bd402c4c5ed8cf958b0f6555fc87086ea33
Sillar-Negro <cerveceriasillarnegro@gmail.com>
2025-05-18 20:21:28 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,13 @@
 const apiKey = 'fb4c0568ab1c59276c47ec15672e639c'; // Reemplaza con tu clave API de API-Football
 
-<div id="wg-api-football-games"
-     data-host="v3.football.api-sports.io"
-     data-key="fb4c0568ab1c59276c47ec15672e639c"
-     data-date=""
-     data-league=""
-     data-season=""
-     data-theme=""
-     data-refresh="15"
-     data-show-toolbar="true"
-     data-show-errors="false"
-     data-show-logos="true"
-     data-modal-game="true"
-     data-modal-standings="true"
-     data-modal-show-logos="true">
+<div id="wg-api-football-game"
+    data-host="v3.football.api-sports.io"
+    data-key="fb4c0568ab1c59276c47ec15672e639c"
+    data-id="718243"
+    data-theme=""
+    data-refresh="15"
+    data-show-errors="false"
+    data-show-logos="true">
 </div>
 <script
     type="module"
